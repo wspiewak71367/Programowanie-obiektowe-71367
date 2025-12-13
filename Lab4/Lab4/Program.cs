46ba53eb06c449b90416ca6ae92f5813749bdf59
wspiewak71367 <wspiewak23@gmail.com>
2025-12-13 13:55:44 +0100

    Add files via upload

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lab4;
+
+class Program
+{
+    static void Main()
+    {
+        List<Shape> shapes = new List<Shape>();
+
+        shapes.Add(new Rectangle { X = 10, Y = 10, Width = 100, Height = 50 });
+        shapes.Add(new Triangle { X = 20, Y = 20, Width = 40, Height = 40 });
+        shapes.Add(new Circle { X = 50, Y = 50, Width = 30, Height = 30 });
+
+        Console.WriteLine("Rysuje figure");
+
+        foreach (Shape shape in shapes)
+        {
+            shape.Draw();
+        }
+    }
+}
---
+++ Lab4/Lab4/Shape.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4
+{
+    public class Shape
+    {
+        public int X {  get; set; }
+        public int Y { get; set; }
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public virtual void Draw()
+        {
+            Console.WriteLine("Nieokreślony kształt");
+        }
+    }
+
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rysuje prostokąt");
+        }
+    }
+    public class Triangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rysuje trójkąt");
+        }
+    }
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rysuje okrąg");
+        }
+    }
+}
